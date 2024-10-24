cc5251aac8f16ba978af837cb0f76c07df3526ec
Gabrielli Tainá <141343996+12Gabrielli@users.noreply.github.com>
2024-10-24 11:43:36 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,4 +1,6 @@
-criaCartao(
+// function riarCartao(categoria,pergunta,resposta){console.log(ategoria,pergunta,resposta;
+
+}
     `programação`
     `o que é HTML?`,
     `È uma linguagem de marcação de hipertexto`,
